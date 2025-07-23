49c2abee80cbe6cc544cda8bbdeb0422f8b4a867
Swosti <swostisambityadav@gmail.com>
2025-07-23 13:30:00 +0530

    Initial Commit

---
+++ PermissionSheet/PermissionSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PermissionSheetApp.swift
+//  PermissionSheet
+//
+//  Created by hb on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PermissionSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
